ad7494a012f889eac629cb63b1efc072362f8083
Jennifer Pazos <jmp521@gmail.com>
2026-05-29 14:59:10 -0500

    add tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_28_235843) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_29_001003) do
   create_table "collections", force: :cascade do |t|
     t.string "color"
     t.datetime "created_at", null: false
@@ -32,6 +32,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_28_235843) do
     t.datetime "updated_at", null: false
     t.string "url"
     t.integer "user_id", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
