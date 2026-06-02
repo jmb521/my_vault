c1833535083b931fcc961e02cfa57b37478a7800
Jennifer Pazos <jmp521@gmail.com>
2026-06-02 17:34:51 -0500

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :collections, dependent: :destroy
+  has_many :links, dependent: :destroy
+  has_many :tags, dependent: :destroy
 end
